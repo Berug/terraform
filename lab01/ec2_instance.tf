1dfe4e94de1a69e39988fb09d928a62af3bc4579
Berug <szulua200@gmail.com>
2021-10-28 09:51:16 -0500

    se cambian algunas variable y se inician instancias con count index

--- lab01/ec2_instance.tf
+++ lab01/ec2_instance.tf
@@ -1,13 +1,16 @@
 resource "aws_instance" "training_instance" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.training_subnet1.id
-  associate_public_ip_address  = true
-  vpc_security_group_ids = [aws_security_group.training_SG.id]
-  private_ip = "10.0.1.10"
-  key_name = "class1"
+  count = length(var.subnet_private_ip)
+  
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.training_subnet1.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.training_SG.id]
+  private_ip                  = element(var.subnet_private_ip, count.index)
+  key_name                    = "class1"
+
   tags = {
-    Name    = "training-instance-1"
+    Name    = "training-instance-${count.index + 1}"
     Project = "Training"
   }
 }
--- lab01/variables.tf
+++ lab01/variables.tf
@@ -1,29 +1,33 @@
 variable "region" {
   description = "region a utilizar en aws"
-  #default     = "us-east-1"
+  #default    = "us-east-1"
 }
 variable "vpc_cdir" {
   description = "cdir de la vpc"
-  #default     = "10.0.0.0/16"
+  #default    = "10.0.0.0/16"
 }
 variable "subnet1_cidr" {
   description = "cdir de la subnet1"
-  #default     = "10.0.1.0/24"
+  #default    = "10.0.1.0/24"
 }
 variable "availability_zone_subnet1" {
   description = "availability zone subnet 1"
-  #default     = "us-east-1a"
+  #default    = "us-east-1a"
 }
 variable "availability_zone_subnet2" {
   description = "availability zone subnet 1"
-  #default     = "us-east-1b"
+  #default    = "us-east-1b"
 }
 variable "availability_zone_subnet3" {
   description = "availability zone subnet 1"
-  #default     = "us-east-1c"
+  #default    = "us-east-1c"
 }
 variable "ami_id" {
   type        = string
   description = "ami id para rhel8"
-  #default     = "ami-02e136e904f3da870"
+  #default    = "ami-02e136e904f3da870"
+}
+variable "subnet_private_ip" {
+  type    = list
+  default = ["192.168.10.10", "192.168.10.11", "192.168.10.12"]
 }
